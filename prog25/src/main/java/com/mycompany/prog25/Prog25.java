eedfafc0906c4197e189e71e46d79fa27560f65b
EL MEJOR <153938220+aadriann12@users.noreply.github.com>
2025-03-07 15:14:10 +0100

    7/3

---
+++ prog25/src/main/java/com/mycompany/prog25/Prog25.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prog25;
+
+/**
+ *
+ * @author adria
+ */
+public class Prog25 {
+
+    public static void main(String[] args) {
+     
+    }
+}
---
+++ prog25/src/main/java/ej7/Alarma.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ej7;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author adria
+ */
+public class Alarma extends Servicio{
+    protected int numAlarmas;
+
+    public Alarma(int numAlarmas, String trabajador, LocalDate fechaInicio, String cliente) {
+        super(trabajador, fechaInicio, cliente);
+        this.numAlarmas = numAlarmas;
+    }
+
+    public int getNumAlarmas() {
+        return numAlarmas;
+    }
+
+    public void setNumAlarmas(int numAlarmas) {
+        this.numAlarmas = numAlarmas;
+    }
+
+ 
+    @Override
+    public double costeManoObra() {
+return(numAlarmas/3)*40;    }
+
+    @Override
+    public double costeTotal() {
+return costeManoObra();
+        }
+
+    @Override
+    public String detalleServicio() {
+        return super.toString()+"Alarma{" + "numAlarmas=" + numAlarmas + '}';
+    }
+
+}
