6c9f644e133163d5af0c7d470dcb9fffdcfcd813
Adrian Teran <aterans03@educantabria.es>
2025-03-11 17:37:42 +0100

    tema6

---
+++ ej1/src/main/java/com/mycompany/ej1/Ej1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ej1;
+
+/**
+ *
+ * @author DAW128
+ */
+public class Ej1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
